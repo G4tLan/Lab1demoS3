3855e74b48bdab16aedd3602dab42d49415f86a9
G4tLan <1043882@students.wits.ac.za>
2018-03-10 19:48:09 +0200

    added lab2 npm express modules

---
+++ Express/index.js
@@ -0,0 +1,9 @@
+let path = require('path');
+let express = require('express');
+let mainRouter = require('./mainRoutes');
+let app = express();
+
+app.use('/',mainRouter);
+		
+app.listen(3000);
+console.log("Express server running on port 3000");

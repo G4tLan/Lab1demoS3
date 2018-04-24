cbf53106d661ce10522941e8315d8a72b4db343b
G4tLan <1043882@students.wits.ac.za>
2018-04-25 00:15:31 +0200

    mocha and tape

--- todoRoutes.js
+++ todoRoutes.js
@@ -1,7 +1,7 @@
 let path = require('path');
 let express = require('express');
 let router = express.Router();
-let todoList = [];
+let todoList = require('./todoList');
 
 router.get('/', function(req,res){
 	res.sendFile(path.join(__dirname, 'views', 'todo', 'index.html'));
@@ -21,26 +21,28 @@ router.get('/edit', function(req,res){
 
 //RESTful api
 router.get('/api/list',function(req,res){
-	res.json(todoList); //Respond with json
+	todoList.show(res); //Respond with json
 });
 
 router.get('/api/get/:id',function(req,res){
-	res.json(todoList[req.params.id]);
+	res.json(todoList.get(req.params.id));
 });
 
 router.post('/api/create',function(req,res){
 	console.log("creating a todo entry", req.body.todo); 
-	todoList.push(req.body.todo);
+	todoList.add(req.body.todo);
 	res.redirect(req.baseUrl+'/api/list');
 });
 
 router.post('/api/delete',function(req,res){
 	console.log("deleting a todo entry"); 
-	delete todoList[req.body.todo];
+	todoList.delete(req.body.todo);
 	res.redirect(req.baseUrl+'/api/list');
 });
 
 router.post('/api/edit',function(req,res){
 	console.log("edit a todo entry"); 
+	todoList.edit(req.body.todo,req.body.todo);
 });
+
 module.exports = router;
